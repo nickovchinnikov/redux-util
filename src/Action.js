ec3f8973e6b00ac33d8b4426ab3ff11a4df35e71
Nick Masters <master-7@inbox.ru>
2017-04-16 17:46:07 +0300

    Drop generic action

--- src/Action.js
+++ src/Action.js
@@ -18,25 +18,3 @@ export const buildActionCreator = (type: string, ...argNames: Array<string>) =>
             ...combinedParams
         };
     };
-
-/**
- * Create generic action
- * @param type
- * @param argNames
- * @returns {Function} function $actionCreator(genericType)(...args)
- */
-export const buildActionCreatorGeneric = (type: string, ...argNames: Array<string>) =>
-    (genericType: string) =>
-        (...args: Array<string>) => {
-            let action = {
-                type: `${type}_:${genericType}`
-            };
-            const combinedParams = R.zipObj(
-                argNames,
-                args
-            );
-            return {
-                action,
-                ...combinedParams
-            };
-        };
